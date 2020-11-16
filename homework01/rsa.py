62cf59979432b641170f0b42837f7f87b695bd1a
Максим Шевченко <maximik02@gmail.com>
2020-11-16 22:48:42 +0300

    Реализован шифр RSA

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,76 +1,76 @@
 import random
 import typing as tp
-
-
-def is_prime(n: int) -> bool:
-    """
-    Tests to see if a number is prime.
-
-    >>> is_prime(2)
-    True
-    >>> is_prime(11)
-    True
-    >>> is_prime(8)
-    False
-    """
-    # PUT YOUR CODE HERE
-    pass
-
-
+import math 
+ 
+def is_prime(n: int) -> bool:  
+ if(n == 1):
+  return False
+ counter = 2
+ while(n % counter != 0):
+  counter += 1
+ if(counter == n):
+  return True
+ else:
+  return False
+ 
+ 
 def gcd(a: int, b: int) -> int:
-    """
-    Euclid's algorithm for determining the greatest common divisor.
-
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
-    # PUT YOUR CODE HERE
-    pass
-
-
+ if (b == 0):
+  return a
+ else:
+  return gcd(b, a % b)
+ 
+ 
 def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    Euclid's extended algorithm for finding the multiplicative
-    inverse of two numbers.
-
-    >>> multiplicative_inverse(7, 40)
-    23
-    """
-    # PUT YOUR CODE HERE
-    pass
-
-
+ var1 = e
+ var2 = phi
+ if var1 < var2:
+  var1, var2 = var2, var1
+ list1 = [var1]
+ list2 = [var2]
+ n = 0
+ while var1 % var2:
+  var1, var2 = var2, var1 % var2
+  list1.append(var1)
+  list2.append(var2)
+  n += 1
+ x, y = 0, 1
+ while n:
+  n -= 1
+  x, y = y, x - y * (list1[n] // list2[n])
+ 
+ return y % phi
+ 
+ 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
-
-    # n = pq
+ 
+    n = p*q
     # PUT YOUR CODE HERE
-
-    # phi = (p-1)(q-1)
+ 
+    phi = (p-1)*(q-1)
     # PUT YOUR CODE HERE
-
+ 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
-
+ 
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-
+ 
     # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-
+ 
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
-
-
+ 
+ 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     # Unpack the key into it's components
     key, n = pk
@@ -79,8 +79,8 @@ def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     cipher = [(ord(char) ** key) % n for char in plaintext]
     # Return the array of bytes
     return cipher
-
-
+ 
+ 
 def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
@@ -88,8 +88,8 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     plain = [chr((char ** key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
-
-
+ 
+ 
 if __name__ == "__main__":
     print("RSA Encrypter/ Decrypter")
     p = int(input("Enter a prime number (17, 19, 23, etc): "))
