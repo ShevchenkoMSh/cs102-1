a995b00272a262a724b9c44fc104960ad90b6e53
Максим Шевченко <maximik02@gmail.com>
2020-11-16 22:43:10 +0300

    Реализован шифр Виженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,32 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
-
-
+ k = 0
+ ciphertext = ""
+ for i in range(len(plaintext)):
+  if (ord(plaintext[i]) >= 65 and ord(plaintext[i]) <= 90):
+   ciphertext += chr((ord(plaintext[i]) + (ord(keyword[k]) % 65) - 65) % 26 + 65)
+   k = (k + 1) % len(keyword)
+  elif (ord(plaintext[i]) >= 97 and ord(plaintext[i]) <= 122):
+   ciphertext += chr((ord(plaintext[i]) + (ord(keyword[k]) % 97) - 97) % 26 + 97)
+   k = (k + 1) % len(keyword)
+  else:
+   ciphertext += plaintext[i]
+   k = (k + 1) % len(keyword)
+ 
+ return ciphertext
+ 
+ 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+ k = 0
+ plaintext = ""
+ for i in range(len(ciphertext)):
+  if (ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 90):
+   plaintext += chr((ord(ciphertext[i]) - ((ord(keyword[k]) % 65) % 65) - 65) % 26 + 65)
+   k = (k + 1) % len(keyword)
+  elif (ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 122):
+   plaintext += chr((ord(ciphertext[i]) - (ord(keyword[k]) % 97) - 97) % 26 + 97)
+   k = (k + 1) % len(keyword)
+  else:
+   plaintext += ciphertext[i]
+   k = (k + 1) % len(keyword)
+ 
+ return plaintext
