4b28534fa78bb8a511ca42a24da3335476f9c07e
Максим Шевченко <maximik02@gmail.com>
2020-11-16 22:22:15 +0300

    Реализован шифр Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,30 @@
 import typing as tp
-
-
+ 
+ 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
-
-
+ 
+ ciphertext = ""
+ for i in range(len(plaintext)):
+  if (ord(plaintext[i]) >= 65 and ord(plaintext[i]) <= 90):
+   ciphertext += chr((ord(plaintext[i]) + shift - 65) % 26 + 65)
+  elif (ord(plaintext[i]) >= 97 and ord(plaintext[i]) <= 122):
+   ciphertext += chr((ord(plaintext[i]) + shift - 97) % 26 + 97)
+  else:
+   ciphertext += plaintext[i]
+ 
+ return ciphertext
+ 
+ 
+ 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
-
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+ 
+ plaintext = ""
+ for i in range(len(ciphertext)):
+  if (ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 90):
+   plaintext += chr((ord(ciphertext[i]) - shift - 65) % 26 + 65)
+  elif (ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 122):
+   plaintext += chr((ord(ciphertext[i]) - shift - 97) % 26 + 97)
+  else:
+   plaintext += ciphertext[i]
+ 
+ return plaintext
