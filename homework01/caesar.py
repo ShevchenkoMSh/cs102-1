68c1c6b752ab0ad102068b2b7ab7ac9c34dd933f
Максим Шевченко <maximik02@gmail.com>
2020-11-22 17:56:28 +0300

    Last version

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -4,14 +4,16 @@ import typing as tp
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     firstBigSymbol = 65
     firstSmallSymbol = 97
-    lastSmallSymbol = 122 
+    lastSmallSymbol = 122
     lastBigSymbol = 90
     ciphertext = ""
     for i in range(len(plaintext)):
         if ord(plaintext[i]) >= firstBigSymbol and ord(plaintext[i]) <= lastBigSymbol:
             ciphertext += chr((ord(plaintext[i]) + shift - firstBigSymbol) % 26 + firstBigSymbol)
         elif ord(plaintext[i]) >= firstSmallSymbol and ord(plaintext[i]) <= lastSmallSymbol:
-            ciphertext += chr((ord(plaintext[i]) + shift - firstSmallSymbol) % 26 + firstSmallSymbol)
+            ciphertext += chr(
+                (ord(plaintext[i]) + shift - firstSmallSymbol) % 26 + firstSmallSymbol
+            )
         else:
             ciphertext += plaintext[i]
 
@@ -25,7 +27,9 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         if ord(ciphertext[i]) >= firstBigSymbol and ord(ciphertext[i]) <= lastBigSymbol:
             plaintext += chr((ord(ciphertext[i]) - shift - firstBigSymbol) % 26 + firstBigSymbol)
         elif ord(ciphertext[i]) >= firstSmallSymbol and ord(ciphertext[i]) <= lastSmallSymbol:
-            plaintext += chr((ord(ciphertext[i]) - shift - firstSmallSymbol) % 26 + firstSmallSymbol)
+            plaintext += chr(
+                (ord(ciphertext[i]) - shift - firstSmallSymbol) % 26 + firstSmallSymbol
+            )
         else:
             plaintext += ciphertext[i]
 
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -2,15 +2,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     k = 0
     firstBigSymbol = 65
     firstSmallSymbol = 97
-    lastSmallSymbol = 122 
+    lastSmallSymbol = 122
     lastBigSymbol = 90
     ciphertext = ""
     for i in range(len(plaintext)):
         if ord(plaintext[i]) >= firstBigSymbol and ord(plaintext[i]) <= lastBigSymbol:
-            ciphertext += chr((ord(plaintext[i]) + (ord(keyword[k]) % firstBigSymbol) - firstBigSymbol) % 26 + firstBigSymbol)
+            ciphertext += chr(
+                (ord(plaintext[i]) + (ord(keyword[k]) % firstBigSymbol) - firstBigSymbol) % 26
+                + firstBigSymbol
+            )
             k = (k + 1) % len(keyword)
         elif ord(plaintext[i]) >= firstSmallSymbol and ord(plaintext[i]) <= lastSmallSymbol:
-            ciphertext += chr((ord(plaintext[i]) + (ord(keyword[k]) % firstSmallSymbol) - firstSmallSymbol) % 26 + firstSmallSymbol)
+            ciphertext += chr(
+                (ord(plaintext[i]) + (ord(keyword[k]) % firstSmallSymbol) - firstSmallSymbol) % 26
+                + firstSmallSymbol
+            )
             k = (k + 1) % len(keyword)
         else:
             ciphertext += plaintext[i]
@@ -24,10 +30,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
     for i in range(len(ciphertext)):
         if ord(ciphertext[i]) >= firstBigSymbol and ord(ciphertext[i]) <= lastBigSymbol:
-            plaintext += chr((ord(ciphertext[i]) - ((ord(keyword[k]) % firstBigSymbol) % firstBigSymbol) - firstBigSymbol) % 26 + firstBigSymbol)
+            plaintext += chr(
+                (
+                    ord(ciphertext[i])
+                    - ((ord(keyword[k]) % firstBigSymbol) % firstBigSymbol)
+                    - firstBigSymbol
+                )
+                % 26
+                + firstBigSymbol
+            )
             k = (k + 1) % len(keyword)
         elif ord(ciphertext[i]) >= firstSmallSymbol and ord(ciphertext[i]) <= lastSmallSymbol:
-            plaintext += chr((ord(ciphertext[i]) - (ord(keyword[k]) % firstSmallSymbol) - firstSmallSymbol) % 26 + firstSmallSymbol)
+            plaintext += chr(
+                (ord(ciphertext[i]) - (ord(keyword[k]) % firstSmallSymbol) - firstSmallSymbol) % 26
+                + firstSmallSymbol
+            )
             k = (k + 1) % len(keyword)
         else:
             plaintext += ciphertext[i]
